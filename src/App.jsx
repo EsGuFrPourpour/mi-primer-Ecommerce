5f967bf1fd96700b95493b53d12caed51973f6bb
Francisco <guillermo.pourpour@hotmail.com>
2025-03-11 20:06:22 -0300

    añadiendo ultimos conocimientos aprendidos en la clase 2 para realizar la entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Navbar from './components/navBar/navBar';
+import ItemListContainer from './components/Carrito/ItemListContainer';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <Navbar/>
+    <ItemListContainer mensaje="¡Bienvenidos a nuestra tienda!"/>
     </>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,21 @@
 import { useState } from 'react';
 import './Navbar.css';
+import CartWidget from '../Carrito/CartWidget';
 
 function Navbar() {
   const [count, setCount] = useState(0);
 
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">logo</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link" href="#">Carrito</a>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">logo</a>
+      <div className="navbar-nav">
+        <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
       </div>
-    </div>
+      <CartWidget/>
   </div>
 </nav>
   );
